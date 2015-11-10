a5a6377ca0c285fa995c1ebdc3ff77ac2ae53eb7
Eric Taïeb Walch <eric@teknologism.com>
2015-11-10 17:49:47 +0100

    Change host

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-mongorestore -v --host mongo-service.default.cluster.local --port 27017 --drop /data/dump
+mongorestore -v --host mongo-cluster.default.cluster.local --port 27017 --drop /data/dump
 exec sleep 36000
